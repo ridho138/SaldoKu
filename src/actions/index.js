cbb9b90ec14510b6973b9158d12a2bc668daa461
ridho138 <teguh.afdilla138@gmail.com>
2019-05-13 20:39:54 +0700

    Add REST API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,4 @@
-
-
+import axios from "axios"
 
 export const FETCH_DATA = 'FETCH_DATA'
 export const fetchData = () => {
@@ -8,4 +7,62 @@ export const fetchData = () => {
   }
 }
 
+export const FETCH_LOGIN = 'FETCH_LOGIN'
+export const fetchLogin = () => {
+  return {
+    type: FETCH_LOGIN,
+    payload: apiLogin()
+  }
+}
 
+const apiLogin = () => {
+  const url = "http://codefazz.com:3000/api/Users/login"
+  return axios.post(url)
+    .then(res => {
+      console.log(res)
+      return res.data
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+export const FETCH_SIGNUP = 'FETCH_SIGNUP'
+export const fetchSignup = () => {
+  return {
+    type: FETCH_SIGNUP,
+    payload: apiSignup()
+  }
+}
+
+const apiSignup = () => {
+  const url = "http://codefazz.com:3000/api/Users"
+  return axios.post(url)
+    .then(res => {
+      console.log(res)
+      return res.data
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+export const FETCH_LOGOUT = 'FETCH_LOGOUT'
+export const fetchLogout = () => {
+  return {
+    type: FETCH_LOGOUT,
+    payload: apiLogout()
+  }
+}
+
+const apiLogout = () => {
+  const url = "http://codefazz.com:3000/api/Users/logout?access_token=xxxxx"
+  return axios.post(url)
+    .then(res => {
+      console.log(res)
+      return res.data
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
