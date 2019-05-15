0bfeec72a5220b646574245e4f3b8350611405ac
ridho138 <teguh.afdilla138@gmail.com>
2019-05-15 21:43:00 +0700

    Add Logout Button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 class Home extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>HOME</Text>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Text>HOME</Text>
+        </View>
+        <View style={{ flex: 1, justifyContent: "flex-end", alignItems: "center", paddingBottom: 15 }}>
+          <TouchableOpacity
+            onPress={() => this.loginHandler()}
+            style={styles.buttonContainer}
+          >
+            <Text style={styles.buttonText}>LOGOUT</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  
+  buttonContainer: {
+    backgroundColor: "#000",
+    borderRadius: 30,
+    paddingVertical: 8,
+    width: 150,
+    elevation: 5
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "700"
+  }
+});
+
 
 export default Home;
